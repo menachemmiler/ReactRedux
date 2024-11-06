528a05ef70f4a71ce32a21be805bfd3755c8dfba
menachem <AM0556727820@GMAIL.COM>
2024-11-06 11:49:08 +0200

    m3

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         {/* // צריך לעטוף את כל הקומפוננטות בספק שמכיל את הקובץ שמאחסן את כך הסטייטים */}
         <div className="main">
           <Routes>
-            <Route index element={<Home />} />
+            <Route path="/ReactRedux/" element={<Home />} />
             <Route path="/ReactRedux/counter" element={<Counter />} />
             <Route path="/ReactRedux/user" element={<User />} />
             <Route path="/ReactRedux/toDos" element={<ToDos />} />
