f3174b34821a7861361a5165a7ec3e53917ee6f1
menachem <AM0556727820@GMAIL.COM>
2024-11-06 11:46:49 +0200

    mm

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react()],
+//   base: mode === 'production' ? '/ReactRedux/' : '/',  // base רק בבנייה לפרודקשן
+// }))
+
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/ReactRedux/' : '/',  // base רק בבנייה לפרודקשן
-}))
+  base: "/ReactRedux/"
+})
